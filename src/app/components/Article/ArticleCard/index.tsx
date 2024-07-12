3d086c009314e3aa431bc6c649004898d2b391bf
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-07-12 21:24:23 +0900

    handleclick OK / 外部リンククリックも記録する

--- src/app/components/Article/ArticleCard/index.tsx
+++ src/app/components/Article/ArticleCard/index.tsx
@@ -1,24 +1,34 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import { HomePageArticle } from '../../../../../types/types' // 適切なパスに調整してください
+
+import { handleClickCount } from '../../handleclick'
 
 interface ArticleCardProps {
 	article: HomePageArticle
 }
 
 const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => {
+	const handleClick = () => {
+		handleClickCount(article.id).catch((error) => console.error('Failed to record click:', error))
+	}
+
 	return (
-		<Link href={`/post/${article.id}`} className="block h-full">
-			<div className="bg-white rounded-lg shadow-md overflow-hidden h-full flex flex-col">
-				<div className="relative pb-[56.25%]">
-					<img src={article.image_url} alt={article.title} className="absolute inset-0 w-full h-full object-cover" />
+		<div onClick={() => handleClick()}>
+			<Link href={`/post/${article.id}`} className="block h-full">
+				<div className="bg-white rounded-lg shadow-md overflow-hidden h-full flex flex-col">
+					<div className="relative pb-[56.25%]">
+						<img src={article.image_url} alt={article.title} className="absolute inset-0 w-full h-full object-cover" />
+					</div>
+					<div className="p-4 flex-grow">
+						<h2 className="text-lg font-semibold mb-2 line-clamp-2">{article.title}</h2>
+						<p className="text-sm text-gray-600">{new Date(article.created_at).toLocaleDateString()}</p>
+					</div>
 				</div>
-				<div className="p-4 flex-grow">
-					<h2 className="text-lg font-semibold mb-2 line-clamp-2">{article.title}</h2>
-					<p className="text-sm text-gray-600">{new Date(article.created_at).toLocaleDateString()}</p>
-				</div>
-			</div>
-		</Link>
+			</Link>
+		</div>
 	)
 }
 
