f954815ba41a0b6366ccf3fef90765f6405dacf6
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-07-22 19:17:26 +0900

    clarity追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 				data-website-id={process.env.UMAMI_WEBSITE_ID}
 				strategy="afterInteractive"
 			/>
+			<Script id="microsoft-clarity" strategy="afterInteractive">
+				{`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nbajc9ngtj");
+          `}
+			</Script>
 			<body className={`${noto.className} flex flex-col min-h-screen`}>
 				<Header />
 				<div className="flex-grow flex flex-col md:flex-row">
