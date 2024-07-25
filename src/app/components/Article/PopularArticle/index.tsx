306c07e3bc57d68b52e5c2ca6e9767184ab08628
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-07-25 22:54:58 +0900

    人気動画コンポへリンク

--- src/app/components/Article/PopularArticle/index.tsx
+++ src/app/components/Article/PopularArticle/index.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import { PopularArticle as PopularArticleType } from '../../../../../types/types'
+import Link from 'next/link'
 
 const Carousel = dynamic(() => import('./carousel'), { ssr: false })
 
@@ -13,12 +14,14 @@ interface PopularArticleProps {
 const PopularArticle: React.FC<PopularArticleProps> = ({ articles }) => {
 	return (
 		<section className="container mx-auto px-1 py-8" aria-labelledby="popular-articles-heading">
-			<h3
-				id="popular-articles-heading"
-				className="text-3xl font-bold text-center mb-6 bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text"
-			>
-				今日の人気動画
-			</h3>
+			<Link href="/popular">
+				<h3
+					id="popular-articles-heading"
+					className="text-3xl font-bold text-center mb-6 bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text"
+				>
+					今日の人気動画
+				</h3>
+			</Link>
 			<Carousel articles={articles} />
 		</section>
 	)
