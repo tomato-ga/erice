b5505b8703f3762ab253af72615bd1b2abf997bb
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-08-13 03:22:10 +0900

    修正中 キャッシュ

--- src/app/components/dmmcomponents/DMMItemContainer.tsx
+++ src/app/components/dmmcomponents/DMMItemContainer.tsx
@@ -30,6 +30,7 @@ async function fetchData(itemType: ItemType): Promise<DMMItemProps[]> {
 			throw new Error(`Invalid itemType: ${itemType}`)
 	}
 
+	// todo revalidatepathなどのアクションが必要そう
 	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, { cache: 'no-store' })
 	const data: DMMItemProps[] = await response.json()
 	return data
