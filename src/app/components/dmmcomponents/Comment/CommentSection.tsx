6dfabce2d9c3fbbb7d24fa37aba549a817ec9314
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-08-15 23:03:35 +0900

    コメント

--- src/app/components/dmmcomponents/Comment/CommentSection.tsx
+++ src/app/components/dmmcomponents/Comment/CommentSection.tsx
@@ -17,7 +17,8 @@ export function CommentSection({ contentId }: { contentId: string }) {
 	return (
 		<section className="bg-gradient-to-br from-pink-100 to-orange-50 dark:from-yellow-900 dark:to-orange-900 rounded-xl shadow-lg p-6 my-8 transform">
 			<h2 className="text-3xl font-extrabold mb-4 text-center bg-gradient-to-r from-pink-500 to-orange-600 text-transparent bg-clip-text">
-				もし動画を見たら、ヌケたシーンを教えてください
+				<span className="block">もし動画を見たら</span>
+				<span className="block">ヌケたシーンを教えてください</span>
 			</h2>
 			<p className="text-gray-700 dark:text-gray-300 text-center mb-6">
 				動画のどんなシーンが印象的だったか、どんなシーンが抜きポイントだったか、ぜひ教えてください
