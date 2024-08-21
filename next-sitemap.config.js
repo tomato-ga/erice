38d9f7eb75d042fdc86a7555b6fefb6f2c0cb4f5
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-08-21 15:27:42 +0900

    robots修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: 'https://erice.cloud', // 実際のサイトURLに置き換えてください
-	generateRobotsTxt: true,
+	// サイトのベースURL
+	siteUrl: 'https://erice.cloud',
+	// robots.txtは手動で配置済みのため、生成しない
+	generateRobotsTxt: false,
+	// 動的生成されるサイトマップインデックスを除外
 	exclude: ['/server-sitemap-index.xml'],
+	// robots.txtに追加するサイトマップ
 	robotsTxtOptions: {
 		additionalSitemaps: ['https://erice.cloud/server-sitemap-index.xml']
-	}
+	},
+	// 各サイトマップのエントリー数（他のファイルと一致させる）
+	sitemapSize: 20000
 }
