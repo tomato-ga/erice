4b0d5880302f8e1529d6ddf0d7886db9108c772b
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-08-27 00:03:17 +0900

    日付fix

--- src/utils/dmmUtils.ts
+++ src/utils/dmmUtils.ts
@@ -2,3 +2,8 @@ export function formatDate(dateString: string): string {
 	const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
 	return new Date(dateString).toLocaleDateString('ja-JP', options)
 }
+
+export function formatMMDDDate(dateString: string): string {
+	const options: Intl.DateTimeFormatOptions = { month: 'numeric', day: 'numeric' }
+	return new Date(dateString).toLocaleDateString('ja-JP', options)
+}
