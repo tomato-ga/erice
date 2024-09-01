ad5cd658f721a21b0852068ac5fb60305d6e0167
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-09-01 14:39:07 +0900

    console delete

--- src/app/components/dmmcomponents/UmamiTracking/index.tsx
+++ src/app/components/dmmcomponents/UmamiTracking/index.tsx
@@ -7,12 +7,12 @@ import { UmamiTrackingProps, UmamiClickData, validateUmamiTrackingData } from '@
 export function UmamiTracking({ trackingData, children }: UmamiTrackingProps) {
 	const handleClick = useCallback(
 		(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-			console.log('UmamiTracking: Original tracking data:', JSON.stringify(trackingData, null, 2))
+			// console.log('UmamiTracking: Original tracking data:', JSON.stringify(trackingData, null, 2))
 			if (validateUmamiTrackingData(trackingData)) {
 				console.log('UmamiTracking: Validated tracking data')
 				handleericeUmamiClick(trackingData)
 			} else {
-				console.error('UmamiTracking: Invalid tracking data', JSON.stringify(trackingData, null, 2))
+				// console.error('UmamiTracking: Invalid tracking data', JSON.stringify(trackingData, null, 2))
 			}
 		},
 		[trackingData]
