080625d742ff04c33ab74429a28663c55599ff69
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-09-07 20:20:49 +0900

    doujin top ok

--- src/app/(doujin)/doujin/page.tsx
+++ src/app/(doujin)/doujin/page.tsx
@@ -7,16 +7,60 @@ interface HomePageProps {
 export default function DoujinHomePage({ searchParams }: HomePageProps) {
 	return (
 		<>
-			<section className='space-y-16 py-12'>
+			<section className="space-y-16 py-12">
 				<DMMDoujinFeaturedItemContainer
-					from='top'
-					bgGradient='bg-gradient-to-r from-blue-50 to-purple-50'
-					endpoint='/api/doujin-get-top-newrank-items'
+					from="top"
+					bgGradient="bg-gradient-to-r from-blue-50 to-purple-50"
+					endpoint="/api/doujin-get-top-sale-items"
 					title={titles.sale}
 					linkText={linkTexts.sale}
-					linkHref='/doujin-newrank'
-					umamifrom='top-sale'
-					textGradient='from-blue-500 to-purple-500'
+					linkHref="/doujin-sale"
+					umamifrom="top-doujin-sale"
+					textGradient="from-blue-500 to-purple-500"
+				/>
+
+				<DMMDoujinFeaturedItemContainer
+					from="top"
+					bgGradient="bg-gradient-to-r from-blue-50 to-purple-50"
+					endpoint="/api/doujin-get-top-newrank-items"
+					title={titles.newrank}
+					linkText={linkTexts.newrank}
+					linkHref="/doujin-sale"
+					umamifrom="top-doujin-sale"
+					textGradient="from-blue-500 to-purple-500"
+				/>
+
+				<DMMDoujinFeaturedItemContainer
+					from="top"
+					bgGradient="bg-gradient-to-r from-blue-50 to-purple-50"
+					endpoint="/api/doujin-get-top-newrelease-items"
+					title={titles.newrelease}
+					linkText={linkTexts.newrelease}
+					linkHref="/doujin-sale"
+					umamifrom="top-doujin-sale"
+					textGradient="from-blue-500 to-purple-500"
+				/>
+
+				<DMMDoujinFeaturedItemContainer
+					from="top"
+					bgGradient="bg-gradient-to-r from-blue-50 to-purple-50"
+					endpoint="/api/doujin-get-top-review-items"
+					title={titles.review}
+					linkText={linkTexts.review}
+					linkHref="/doujin-sale"
+					umamifrom="top-doujin-sale"
+					textGradient="from-blue-500 to-purple-500"
+				/>
+
+				<DMMDoujinFeaturedItemContainer
+					from="top"
+					bgGradient="bg-gradient-to-r from-blue-50 to-purple-50"
+					endpoint="/api/doujin-get-top-popular-circles-items"
+					title={titles.circle}
+					linkText={linkTexts.circle}
+					linkHref="/doujin-sale"
+					umamifrom="top-doujin-sale"
+					textGradient="from-blue-500 to-purple-500"
 				/>
 			</section>
 		</>
@@ -24,28 +68,28 @@ export default function DoujinHomePage({ searchParams }: HomePageProps) {
 }
 
 const gradients = {
-	todaynew: 'from-green-500 to-blue-500',
+	newrank: 'from-green-500 to-blue-500',
 	debut: 'from-yellow-500 to-red-500',
 	feature: 'from-pink-500 to-purple-500',
 	sale: 'from-blue-500 to-purple-500',
 	actress: 'from-blue-500 to-purple-500',
-	genre: 'from-blue-500 to-purple-500',
+	genre: 'from-blue-500 to-purple-500'
 }
 
 const titles = {
-	todaynew: '今日配信の新作',
-	debut: 'デビュー作品',
-	feature: '注目作品',
+	newrank: '人気作品',
+	newrelease: '新着作品',
+	review: '評価の高い作品',
 	sale: '限定セール',
-	actress: 'アクトレス',
-	genre: 'ジャンル',
+	circle: '人気サークル',
+	genre: 'ジャンル'
 }
 
 const linkTexts = {
-	todaynew: '全ての新作商品を見る',
-	debut: '全てのデビュー作品を見る',
-	feature: '全ての注目作品を見る',
+	newrank: '全ての人気商品を見る',
+	newrelease: '全ての新着作品を見る',
+	review: '全ての評価の高い作品を見る',
 	sale: '全ての限定セール商品を見る',
-	actress: '全てのアクトレスを見る',
-	genre: '全てのジャンルを見る',
+	circle: '全ての人気サークルを見る',
+	genre: '全てのジャンルを見る'
 }
