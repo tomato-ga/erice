603ffd5dae2ead35696f0a1f593f6705530e738e
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-09-17 01:55:48 +0900

    fix

--- src/app/(pages)/todaynew/page.tsx
+++ src/app/(pages)/todaynew/page.tsx
@@ -13,8 +13,9 @@ export default function DMMGenericPage() {
 		debut: 'デビュー作品',
 		feature: '注目作品',
 		sale: '限定セール',
+		last7days: '過去7日間の新作',
 		actress: 'アクトレス',
-		genre: 'ジャンル'
+		genre: 'ジャンル',
 	}
 
 	const gradients: Record<ItemType, { bg: string; text: string }> = {
@@ -23,27 +24,30 @@ export default function DMMGenericPage() {
 		feature: { bg: 'from-pink-50 to-purple-50', text: 'from-pink-500 to-purple-500' },
 		sale: { bg: 'from-blue-50 to-purple-50', text: 'from-blue-500 to-purple-500' },
 		actress: { bg: 'from-blue-50 to-purple-50', text: 'from-red-500 to-blue-500' },
-		genre: { bg: 'from-blue-50 to-purple-50', text: 'from-red-500 to-blue-500' }
+		genre: { bg: 'from-blue-50 to-purple-50', text: 'from-red-500 to-blue-500' },
+		last7days: { bg: 'from-yellow-50 to-red-50', text: 'from-yellow-500 to-red-500' },
 	}
 
 	if (!Object.keys(pageTitles).includes(itemType)) {
 		return (
-			<div className="container mx-auto px-4 py-12">
-				<h1 className="text-3xl font-bold text-red-600 text-center mb-4">無効な itemType です</h1>
-				<p className="text-center">有効な itemType は `todaynew`, `debut`, `feature`, `sale` です。</p>
+			<div className='container mx-auto px-4 py-12'>
+				<h1 className='text-3xl font-bold text-red-600 text-center mb-4'>無効な itemType です</h1>
+				<p className='text-center'>
+					有効な itemType は `todaynew`, `debut`, `feature`, `sale` です。
+				</p>
 			</div>
 		)
 	}
 
 	return (
-		<div className="w-full">
-			<div className="w-full">
+		<div className='w-full'>
+			<div className='w-full'>
 				<DMMFeaturesItemContainer
-					from="only"
+					from='only'
 					bgGradient={`bg-gradient-to-r ${gradients[itemType].bg}`}
 					endpoint={`/api/dmm-${itemType}-getkv`}
 					title={pageTitles[itemType]}
-					linkText="すべて見る"
+					linkText='すべて見る'
 					linkHref={`/${itemType}` as '/sale' | '/todaynew' | '/debut' | '/feature'}
 					textGradient={gradients[itemType].text}
 					umamifrom={`only-${itemType}`}
