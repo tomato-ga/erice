9ac7a20a02d97c529789b91568e0354aac04eb17
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-09-22 20:36:13 +0900

    gradient button

--- src/app/(pages)/item/[dbId]/page.tsx
+++ src/app/(pages)/item/[dbId]/page.tsx
@@ -10,7 +10,7 @@ import {
 	fetchItemDetailByContentId,
 	fetchItemMainByContentId,
 	fetchItemMainByContentIdToActressInfo,
-	fetchRelatedItems
+	fetchRelatedItems,
 } from '@/app/components/dmmcomponents/fetch/itemFetchers'
 import { DMMItemMainResponse } from '@/types/dmmitemzodschema'
 import { ExtendedDMMItem, ItemType } from '@/types/dmmtypes'
@@ -20,6 +20,7 @@ import { Metadata } from 'next'
 
 import Link from 'next/link'
 import { Suspense } from 'react'
+import './styles.css'
 
 interface Props {
 	params: { dbId: number }
@@ -27,8 +28,8 @@ interface Props {
 
 function LoadingSpinner() {
 	return (
-		<div className="flex justify-center items-center h-64" aria-label="読み込み中">
-			<div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900" />
+		<div className='flex justify-center items-center h-64' aria-label='読み込み中'>
+			<div className='animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900' />
 		</div>
 	)
 }
@@ -70,16 +71,16 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 					url: 'https://erocomesuto.com/ogp.jpg',
 					width: 1200,
 					height: 630,
-					alt: 'エロコメスト OGP画像'
-				}
-			]
+					alt: 'エロコメスト OGP画像',
+				},
+			],
 		},
 		twitter: {
 			card: 'summary_large_image',
 			title,
 			description,
-			images: ['https://erocomesuto.com/ogp.jpg']
-		}
+			images: ['https://erocomesuto.com/ogp.jpg'],
+		},
 	}
 }
 
@@ -106,15 +107,15 @@ const VideoPlayer = ({ src }: { src: string | null | undefined }) => {
 	const height = size?.height || 480 // デフォルト値
 
 	return (
-		<div className="video-player-container flex justify-center items-center my-8">
-			<div className="max-w-full">
+		<div className='video-player-container flex justify-center items-center my-8'>
+			<div className='max-w-full'>
 				<iframe
 					src={src}
 					width={width}
 					height={height}
-					allow="autoplay"
-					title="動画プレイヤー"
-					className="border-0 overflow-hidden max-w-full"
+					allow='autoplay'
+					title='動画プレイヤー'
+					className='border-0 overflow-hidden max-w-full'
 				/>
 			</div>
 		</div>
@@ -123,7 +124,7 @@ const VideoPlayer = ({ src }: { src: string | null | undefined }) => {
 
 export default async function DMMKobetuItemPage({
 	params,
-	searchParams
+	searchParams,
 }: Props & { searchParams: { itemType?: ItemType } }) {
 	// console.log('Received params:', params)
 	// console.log('itemType:', searchParams.itemType)
@@ -141,9 +142,10 @@ export default async function DMMKobetuItemPage({
 
 	if (!ItemMain) {
 		return (
-			<div className="container mx-auto px-2 py-6">
-				<h1 className="text-2xl font-bold text-red-600">
-					{searchParams.itemType ? searchParams.itemType : '指定された'}のアイテムが見つかりませんでした
+			<div className='container mx-auto px-2 py-6'>
+				<h1 className='text-2xl font-bold text-red-600'>
+					{searchParams.itemType ? searchParams.itemType : '指定された'}
+					のアイテムが見つかりませんでした
 				</h1>
 				<p>アイテムが存在しないか、取得中にエラーが発生しました。</p>
 			</div>
@@ -152,89 +154,111 @@ export default async function DMMKobetuItemPage({
 
 	const relatedItemTypes: ItemType[] = ['todaynew', 'debut', 'feature', 'sale']
 	const relatedItemsData = await Promise.all(
-		relatedItemTypes.map(async (type) => ({
+		relatedItemTypes.map(async type => ({
 			type,
-			items: (await fetchRelatedItems(type)) as ExtendedDMMItem[]
-		}))
+			items: (await fetchRelatedItems(type)) as ExtendedDMMItem[],
+		})),
 	)
 
 	return (
-		<div className="bg-gray-50 dark:bg-gray-900 min-h-screen">
-			<div className="container mx-auto px-2 sm:px-4 py-6 sm:py-8">
-				<article className="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 sm:p-6 space-y-6 sm:space-y-8">
+		<div className='bg-gray-50 dark:bg-gray-900 min-h-screen'>
+			<div className='container mx-auto px-2 sm:px-4 py-6 sm:py-8'>
+				<article className='bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 sm:p-6 space-y-6 sm:space-y-8'>
 					<PostList limit={12} />
 
-					<div className="relative overflow-hidden aspect-w-16 aspect-h-9">
+					<div className='relative overflow-hidden aspect-w-16 aspect-h-9'>
 						<UmamiTracking
 							trackingData={{
 								dataType: 'combined',
 								from: 'kobetu-img-top',
 								item: ItemMain,
-								actressInfo: actressInfo
-							}}
-						>
-							<Link href={ItemMain.affiliateURL || '#'} target="_blank" rel="noopener noreferrer">
+								actressInfo: actressInfo,
+							}}>
+							<Link href={ItemMain.affiliateURL || '#'} target='_blank' rel='noopener noreferrer'>
 								<img
 									src={ItemMain.imageURL}
 									alt={`${ItemMain.title}のパッケージ画像`}
-									className="w-full h-full object-contain transition-transform duration-300"
+									className='w-full h-full object-contain transition-transform duration-300'
 								/>
 							</Link>
 						</UmamiTracking>
 					</div>
 
-					<h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800 dark:text-gray-100 text-center">
+					<h1 className='text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800 dark:text-gray-100 text-center'>
 						{ItemMain.title}
 					</h1>
 
-					<div className="flex justify-center">
-						<UmamiTracking
+					<div className='flex justify-center'>
+						{/* <UmamiTracking
 							trackingData={{
 								dataType: 'combined',
 								from: 'kobetu-exlink-top',
 								item: ItemMain,
-								actressInfo: actressInfo
-							}}
-						>
+								actressInfo: actressInfo,
+							}}>
 							<Link
 								href={ItemMain.affiliateURL || '#'}
-								target="_blank"
-								rel="noopener noreferrer"
-								className="inline-flex items-center justify-center text-xl font-semibold text-white bg-gradient-to-r from-indigo-500 to-purple-600 dark:from-indigo-600 dark:to-purple-700 rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 px-6 sm:px-8 py-3 sm:py-4"
-							>
-								<span className="mr-2">高画質動画を見る</span>
-								<ExternalLink className="w-5 h-5 sm:w-6 sm:h-6 animate-pulse" />
+								target='_blank'
+								rel='noopener noreferrer'
+								className='inline-flex items-center justify-center text-xl font-semibold text-white bg-gradient-to-r from-indigo-500 to-purple-600 dark:from-indigo-600 dark:to-purple-700 rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 px-6 sm:px-8 py-3 sm:py-4'>
+								<span className='mr-2'>高画質動画を見る</span>
+								<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
 							</Link>
-						</UmamiTracking>
+						</UmamiTracking> */}
+
+						<div className='relative inline-block group'>
+							{/* グラデーションオーバーレイ */}
+							<div className='absolute inset-[-7%] rounded-full opacity-50 blur-lg group-hover:opacity-100 transition-opacity duration-500 ease-in-out bg-custom-gradient-exbutton' />
+							{/* ボタン */}
+							<UmamiTracking
+								trackingData={{
+									dataType: 'combined',
+									from: 'kobetu-exlink-top',
+									item: ItemMain,
+									actressInfo: actressInfo,
+								}}>
+								<Link
+									href={ItemMain.affiliateURL || '#'}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='relative inline-flex items-center justify-center text-xl font-semibold text-white  rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 px-6 sm:px-8 py-3 sm:py-4 hover:bg-gray-700 transform hover:-translate-y-0.5 bg-custom-gradient-exbutton'>
+									<span className='mr-2'>高画質動画を見る</span>
+									<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
+								</Link>
+							</UmamiTracking>
+						</div>
 					</div>
 
 					<Suspense fallback={<LoadingSpinner />}>
 						<CommentSection contentId={ItemMain.content_id} />
 					</Suspense>
 
-					<div className="w-full text-sm text-center my-4">このページに広告を設置しています</div>
+					<div className='w-full text-sm text-center my-4'>このページに広告を設置しています</div>
 
 					<Suspense fallback={<LoadingSpinner />}>
-						<ProductDetails title={ItemMain.title} contentId={ItemMain.content_id} dbId={params.dbId} />
+						<ProductDetails
+							title={ItemMain.title}
+							contentId={ItemMain.content_id}
+							dbId={params.dbId}
+						/>
 					</Suspense>
 
 					{ItemMain.sampleImageURL && ItemMain.sampleImageURL.length > 0 && (
-						<div className="mt-8">
-							<h2 className="text-center font-bold mb-6">
-								<span className="text-2xl bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text">
+						<div className='mt-8'>
+							<h2 className='text-center font-bold mb-6'>
+								<span className='text-2xl bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text'>
 									サンプル画像
 								</span>
 							</h2>
-							<div className="grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4">
+							<div className='grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4'>
 								{ItemMain.sampleImageURL.map((imageUrl, index) => (
 									<div
 										key={index}
-										className="aspect-w-16 aspect-h-9 relative overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300"
-									>
+										className='aspect-w-16 aspect-h-9 relative overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300'>
 										<img
 											src={imageUrl}
 											alt={`${ItemMain.title} ${ItemMain.content_id}のサンプル画像${index + 1}`}
-											className="w-full h-full object-contain transition-transform duration-300"
+											className='w-full h-full object-contain transition-transform duration-300'
 										/>
 									</div>
 								))}
@@ -243,39 +267,62 @@ export default async function DMMKobetuItemPage({
 					)}
 
 					{ItemMain.sampleMovieURL && ItemMain.sampleMovieURL.length > 0 && (
-						<div className="mt-8">
-							<h2 className="text-center font-bold mb-6">
-								<span className="text-2xl bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text">
+						<div className='mt-8'>
+							<h2 className='text-center font-bold mb-6'>
+								<span className='text-2xl bg-gradient-to-r from-purple-600 to-pink-500 text-transparent bg-clip-text'>
 									サンプル動画
 								</span>
 							</h2>
-							<div className="flex justify-center">
+							<div className='flex justify-center'>
 								<VideoPlayer src={ItemMain.sampleMovieURL[0]} />
 							</div>
 						</div>
 					)}
 
-					<div className="mt-8">
+					{/* <div className='mt-8'>
 						<UmamiTracking
 							trackingData={{
 								dataType: 'combined',
 								from: 'kobetu-exlink-bottom',
 								item: ItemMain,
-								actressInfo: actressInfo
-							}}
-						>
-							<div className="flex justify-center">
+								actressInfo: actressInfo,
+							}}>
+							<div className='flex justify-center'>
 								<Link
 									href={ItemMain.affiliateURL || '#'}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="inline-flex items-center justify-center text-lg sm:text-xl font-semibold text-white bg-gradient-to-r from-indigo-500 to-purple-600 dark:from-indigo-600 dark:to-purple-700 rounded-sm shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 px-6 sm:px-8 py-3 sm:py-4 min-h-[3.5rem] sm:min-h-[4rem] max-w-[90%] text-center"
-								>
-									<span className="mr-2 break-words">{ItemMain.title}の高画質動画を見る</span>
-									<ExternalLink className="w-5 h-5 sm:w-6 sm:h-6 animate-pulse flex-shrink-0" />
+									target='_blank'
+									rel='noopener noreferrer'
+									className='inline-flex items-center justify-center text-lg sm:text-xl font-semibold text-white bg-gradient-to-r from-indigo-500 to-purple-600 dark:from-indigo-600 dark:to-purple-700 rounded-sm shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 px-6 sm:px-8 py-3 sm:py-4 min-h-[3.5rem] sm:min-h-[4rem] max-w-[90%] text-center'>
+									<span className='mr-2 break-words'>{ItemMain.title}の高画質動画を見る</span>
+									<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse flex-shrink-0' />
 								</Link>
 							</div>
 						</UmamiTracking>
+					</div> */}
+
+					<div className='flex justify-center mt-8'>
+						<div className='relative inline-block group'>
+							{/* グラデーションオーバーレイ */}
+							<div className='absolute inset-[-1%] rounded-full opacity-50 blur-lg group-hover:opacity-100 transition-opacity duration-500 ease-in-out bg-custom-gradient-exbutton' />
+
+							{/* ボタン */}
+							<UmamiTracking
+								trackingData={{
+									dataType: 'combined',
+									from: 'kobetu-exlink-bottom',
+									item: ItemMain,
+									actressInfo: actressInfo,
+								}}>
+								<Link
+									href={ItemMain.affiliateURL}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='relative inline-flex items-center justify-center text-xl font-semibold text-white rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 px-6 sm:px-8 py-3 sm:py-4 hover:bg-gray-700 transform hover:-translate-y-0.5 bg-custom-gradient-exbutton'>
+									<span className='mr-2'>{ItemMain.title}の高画質動画を見る</span>
+									<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
+								</Link>
+							</UmamiTracking>
+						</div>
 					</div>
 
 					<Suspense fallback={<LoadingSpinner />}>
@@ -291,10 +338,10 @@ export default async function DMMKobetuItemPage({
 								type === 'todaynew'
 									? '今日配信の新作'
 									: type === 'debut'
-									? 'デビュー作品'
-									: type === 'feature'
-									? '注目作品'
-									: '限定セール'
+										? 'デビュー作品'
+										: type === 'feature'
+											? '注目作品'
+											: '限定セール'
 							}
 						/>
 					))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
+// tailwind.config.ts
+
 import type { Config } from 'tailwindcss'
 
-const config = {
+const config: Config = {
 	darkMode: ['class'],
-	content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+	content: [
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
+	],
 	prefix: '',
 	theme: {
 		container: {
 			center: true,
 			padding: '2rem',
 			screens: {
-				'2xl': '1400px'
-			}
+				'2xl': '1400px',
+			},
 		},
 		extend: {
 			colors: {
@@ -21,60 +28,81 @@ const config = {
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
+					foreground: 'hsl(var(--primary-foreground))',
 				},
 				secondary: {
 					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))'
+					foreground: 'hsl(var(--secondary-foreground))',
 				},
 				destructive: {
 					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))'
+					foreground: 'hsl(var(--destructive-foreground))',
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
+					foreground: 'hsl(var(--muted-foreground))',
 				},
 				accent: {
 					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))'
+					foreground: 'hsl(var(--accent-foreground))',
 				},
 				popover: {
 					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))'
+					foreground: 'hsl(var(--popover-foreground))',
 				},
 				card: {
 					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))'
+					foreground: 'hsl(var(--card-foreground))',
 				},
 				rank: {
 					gold: '#FFD700',
 					silver: '#C0C0C0',
-					bronze: '#CD7F32'
-				}
+					bronze: '#CD7F32',
+				},
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				sm: 'calc(var(--radius) - 4px)',
 			},
 			keyframes: {
 				'accordion-down': {
 					from: { height: '0' },
-					to: { height: 'var(--radix-accordion-content-height)' }
+					to: { height: 'var(--radix-accordion-content-height)' },
 				},
 				'accordion-up': {
 					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: '0' }
-				}
+					to: { height: '0' },
+				},
+				myGradient: {
+					// キーフレーム名を変更
+					'0%': { backgroundPosition: '0% 50%' },
+					'50%': { backgroundPosition: '100% 50%' },
+					'100%': { backgroundPosition: '0% 50%' },
+				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out'
-			}
-		}
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				myGradient: 'myGradient 10s ease infinite', // アニメーション名を変更
+			},
+		},
 	},
-	plugins: [require('tailwindcss-animate')]
+	plugins: [
+		require('tailwindcss-animate'),
+		// カスタムユーティリティクラスの追加
+		({ addUtilities }) => {
+			const newUtilities = {
+				'.bg-custom-gradient-exbutton': {
+					backgroundImage: 'linear-gradient(-45deg, #ffcb47, #e34ba9, #369eff, #95f3d9)',
+					backgroundSize: '600% 600%',
+					backgroundRepeat: 'no-repeat',
+					animation: 'myGradient 10s ease infinite', // 一貫したアニメーション名と期間
+				},
+			}
+			addUtilities(newUtilities, ['responsive', 'hover'])
+		},
+	],
 } satisfies Config
 
 export default config
