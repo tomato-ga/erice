cf4b1c2efdc77555d5b82b443bdc8e0eda579acf
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-09-24 15:42:28 +0900

    key css

--- src/app/(pages)/keywords/[keyword]/page.tsx
+++ src/app/(pages)/keywords/[keyword]/page.tsx
@@ -75,7 +75,7 @@ const ItemDetailsTable: React.FC<{ item: DMMKeywordItemProps; keyword: string }>
 								<Link
 									key={genre.id}
 									href={`/genre/${encodeURIComponent(genre.name)}`}
-									className='bg-pink-100 hover:bg-pink-600 text-pink-500 border-pink-500 mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300'>
+									className='bg-pink-100 hover:bg-pink-600 text-pink-500 border-pink-500 mr-2 px-2.5 py-0.5 rounded dark:text-pink-200 dark:border-pink-400 dark:hover:bg-pink-600 dark:hover:text-white'>
 									{genre.name}
 								</Link>
 							)) || 'N/A'}
--- src/app/(pages)/keywords/page.tsx
+++ src/app/(pages)/keywords/page.tsx
@@ -156,7 +156,7 @@ export default KeywordsPage
 const KeywordButton: React.FC<{ keyword: string; href: string }> = ({ keyword, href }) => (
 	<Link
 		href={href}
-		className='bg-transparent hover:bg-pink-600 text-pink-500 font-semibold hover:text-white py-2 px-4 border border-pink-500 hover:border-transparent rounded dark:text-pink-200 dark:border-pink-400 dark:hover:bg-pink-600 dark:hover:text-white transition-colors duration-300'>
+		className='bg-transparent hover:bg-pink-600 text-pink-500 font-semibold hover:text-white py-1.5 px-2.5 border border-pink-500 hover:border-transparent rounded dark:text-pink-200 dark:border-pink-400 dark:hover:bg-pink-600 dark:hover:text-white transition-colors duration-300'>
 		{keyword}
 	</Link>
 )
