fd5a2309d6859e91662261455e20ac96ecc0a3e3
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-09-25 16:45:46 +0900

    r18はrevalidate 3600にした

--- src/app/components/antennacomponents/PostList/r18fetch.tsx
+++ src/app/components/antennacomponents/PostList/r18fetch.tsx
@@ -1,23 +1,23 @@
 import { antennaPostApiResponse } from '@/types/antennaschema'
 import { unstable_cache } from 'next/cache'
+import { revalidate } from '../../doujincomponents/DMMDoujinFeaturedItemContainer'
 
-export const r18antennaFetch = unstable_cache(
-	async (limit: number): Promise<antennaPostApiResponse> => {
-		try {
-			const response = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/api/r18-latestpost?limit=${limit}`,
-			)
+export const r18antennaFetch = async (limit: number): Promise<antennaPostApiResponse> => {
+	try {
+		const response = await fetch(
+			`${process.env.NEXT_PUBLIC_API_URL}/api/r18-latestpost?limit=${limit}`,
+			{ next: { revalidate: 3600 } },
+		)
 
-			const postsdata: antennaPostApiResponse = await response.json()
+		const postsdata: antennaPostApiResponse = await response.json()
 
-			if (postsdata.status !== 'success') {
-				throw new Error('APIからのレスポンスが不正です')
-			}
-
-			return postsdata
-		} catch (error) {
-			console.error('データの取得に失敗しました:', error)
-			throw error
+		if (postsdata.status !== 'success') {
+			throw new Error('APIからのレスポンスが不正です')
 		}
-	},
-)
+
+		return postsdata
+	} catch (error) {
+		console.error('データの取得に失敗しました:', error)
+		throw error
+	}
+}
