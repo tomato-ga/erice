65e95883413a19dd2fa7c183988b845b5e902e07
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-10-02 23:46:51 +0900

    fix

--- src/app/components/dmmcomponents/ABtest/GradientButton/gradient.tsx
+++ src/app/components/dmmcomponents/ABtest/GradientButton/gradient.tsx
@@ -7,6 +7,7 @@ import { DMMItemMainResponse } from '@/types/dmmitemzodschema'
 import { ExternalLink } from 'lucide-react'
 import Link from 'next/link'
 import { useEffect } from 'react'
+import { UmamiTracking } from '../../UmamiTracking'
 
 interface ButtonWithGradientProps {
 	ItemMain: DMMItemMainResponse
@@ -25,19 +26,27 @@ export const ButtonWithGradient = ({ ItemMain, actressInfo }: ButtonWithGradient
 	}
 
 	return (
-		<div className='flex justify-center'>
-			<div className='relative inline-block group'>
-				<div className='absolute inset-2 rounded-full opacity-70 blur-xl group-hover:opacity-100 transition-opacity duration-500 ease-in-out bg-custom-gradient-exbutton bg-custom-gradient-exbutton--dmm z-0 pointer-events-none' />
-				<Link
-					href={ItemMain.affiliateURL}
-					target='_blank'
-					rel='noopener noreferrer'
-					className='relative z-10 inline-flex items-center justify-center text-xl font-semibold text-white rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 px-6 sm:px-8 py-3 sm:py-4 hover:bg-gray-700 transform hover:-translate-y-0.5 bg-custom-gradient-exbutton bg-custom-gradient-exbutton--dmm'
-					onClick={handleButtonClick}>
-					<span className='mr-2'>高画質動画を見る</span>
-					<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
-				</Link>
+		<UmamiTracking
+			trackingData={{
+				dataType: 'combined',
+				from: 'kobetu-exlink-top',
+				item: ItemMain,
+				actressInfo: actressInfo,
+			}}>
+			<div className='flex justify-center'>
+				<div className='relative inline-block group'>
+					<div className='absolute inset-2 rounded-full opacity-70 blur-xl group-hover:opacity-100 transition-opacity duration-500 ease-in-out bg-custom-gradient-exbutton bg-custom-gradient-exbutton--dmm z-0 pointer-events-none' />
+					<Link
+						href={ItemMain.affiliateURL}
+						target='_blank'
+						rel='noopener noreferrer'
+						className='relative z-10 inline-flex items-center justify-center text-xl font-semibold text-white rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 px-6 sm:px-8 py-3 sm:py-4 hover:bg-gray-700 transform hover:-translate-y-0.5 bg-custom-gradient-exbutton bg-custom-gradient-exbutton--dmm'
+						onClick={handleButtonClick}>
+						<span className='mr-2'>高画質動画を見る</span>
+						<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
+					</Link>
+				</div>
 			</div>
-		</div>
+		</UmamiTracking>
 	)
 }
--- src/app/components/dmmcomponents/ABtest/GradientButton/no-gradient.tsx
+++ src/app/components/dmmcomponents/ABtest/GradientButton/no-gradient.tsx
@@ -7,6 +7,7 @@ import { DMMItemMainResponse } from '@/types/dmmitemzodschema'
 import { ExternalLink } from 'lucide-react'
 import Link from 'next/link'
 import { useEffect } from 'react'
+import { UmamiTracking } from '../../UmamiTracking'
 
 interface ButtonNoGradientProps {
 	ItemMain: DMMItemMainResponse
@@ -25,16 +26,24 @@ export const ButtonNoGradient = ({ ItemMain, actressInfo }: ButtonNoGradientProp
 	}
 
 	return (
-		<div className='flex justify-center'>
-			<Link
-				href={ItemMain.affiliateURL || '#'}
-				target='_blank'
-				rel='noopener noreferrer'
-				className='inline-flex items-center justify-center text-xl font-semibold text-white bg-gradient-to-r from-indigo-500 to-purple-600 dark:from-indigo-600 dark:to-purple-700 rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 px-6 sm:px-8 py-3 sm:py-4'
-				onClick={handleButtonClick}>
-				<span className='mr-2'>高画質動画を見る</span>
-				<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
-			</Link>
-		</div>
+		<UmamiTracking
+			trackingData={{
+				dataType: 'combined',
+				from: 'kobetu-exlink-top',
+				item: ItemMain,
+				actressInfo: actressInfo,
+			}}>
+			<div className='flex justify-center'>
+				<Link
+					href={ItemMain.affiliateURL || '#'}
+					target='_blank'
+					rel='noopener noreferrer'
+					className='inline-flex items-center justify-center text-xl font-semibold text-white bg-gradient-to-r from-indigo-500 to-purple-600 dark:from-indigo-600 dark:to-purple-700 rounded-full shadow-lg transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 px-6 sm:px-8 py-3 sm:py-4'
+					onClick={handleButtonClick}>
+					<span className='mr-2'>高画質動画を見る</span>
+					<ExternalLink className='w-5 h-5 sm:w-6 sm:h-6 animate-pulse' />
+				</Link>
+			</div>
+		</UmamiTracking>
 	)
 }
