8f33aecd6089164532c002dac6f3ed0dd34b21b9
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-10-26 23:21:39 +0900

    add sender info content

---
+++ src/app/(pages)/request_for_disclosure_of_sender_information/page.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const SenderInformation: React.FC = () => {
+	return (
+		<div className='bg-gray-50 dark:bg-gray-900 min-h-screen py-12'>
+			<div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+				<h1 className='text-3xl font-bold text-gray-900 dark:text-gray-100 mb-8'>
+					発信者情報開示請求を受けたときの話
+				</h1>
+
+				<div className='bg-white dark:bg-gray-800 shadow overflow-hidden sm:rounded-lg'>
+					<div className='px-4 py-5 sm:p-6 space-y-6'>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							このサイトを始めようと思ったのは、私が違法ダウンロードをしていた結果、損害賠償請求を個人宛に起こされたことがきっかけです。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							何も悪気がなくエロ動画を検索してはダウンロードし、好きなセクシー女優の動画コレクションをハードディスクにため込んで、コンプリートすることに快感を覚えていました。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							ネットを検索すれば出てくるエロ動画をダウンロードしているだけだし、「別に普通のことだろう」という感覚でいたのがすべての間違いでした。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							プロバイダから届いた「発信者情報の開示等に係わる意見照会書」は、郵便局の配達員が簡易書留で届けてくれました。書留が届くこと自体が普段と違うことから、嫌な予感がします。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							「プロバイダから連絡来ることって何かしたっけな？」と、違法ダウンロードが日常になっていた自分にとっては寝耳に水でしたね。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							封筒の中身を上から読み進めていくと顔が青ざめていきます。ここにその書類内容を記載します。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							拝啓　日頃より○○をご愛顧いただき、厚くお礼申し上げます。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							お客様が発信された情報の流通により権利が侵害されたと主張される方から、当社を相手方として、お客様の発信者情報の開示命令を求める申立てがなされ、東京地方裁判所にて審理が係属しております。そこで、特定電気通信役務提供者の損害賠償責任の制限および発信者情報の開示に関する法律（プロバイダ責任制限法）第6条第1項に鑑み、弊社が開示に応じること等について、お客様のご意見を照会いたします。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							なお、ご回答いただけない場合又は開示に同意されない場合であっても、裁判所が開示を認容する判決を下した場合には、弊社は、お客様の発信者情報を、権利が侵害されたと主張される方に開示いたしますので、その旨ご承知おきください。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							また、回答書につきましては、その内容を裁判所に書庫等として提出させていただくこともありますので、以下のご注意事項を十分お読みの上、ご記入ください。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							ここまで読んで、地方裁判所で審理が進行しているという点。開示に同意しないとしても、判決によってはプロバイダから（強制的に）開示すると書かれていることから、結局は個人情報が開示されてしまう＝自ら開示するしかないと判断しました。つまり、何を選んだとしても結局開示されてしまうわけで、逃げ道はないんだなという印象です。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							ただ、プロバイダからは誠意が感じられる文面で、「あなたは悪くないかもしれないから、個人を特定できることは絶対に書かないでください」という注意書きもあり、有罪認定されているわけじゃないんだなという風にも読める書類でしたね。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							その意見照会書は5枚以上に及ぶもので、自分が何を、いつ、どうやって違法ダウンロードしたのか、詳細に記載されています。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							侵害情報等の項目を読み進めると、「発信者情報の開示を受けるべき正当理由」の欄には、損害賠償請求権の行使のために必要であるためと記載されており、ここで自分が行っていたことは、損害賠償請求の対象であることを認識します。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							なんで調子に乗ってダウンロードしていたんだろうと後悔しました。ほとんどのエロ動画は見もしていなくて、ただのコレクション欲を満たしていただけでした。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							開示請求に対して、発信者情報としては、氏名・住所・メールアドレスが求められます。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							回答書が二枚同封されており、回答書（2）は送付してしまったために記憶しかありませんが、何かしら記入をして返送した記憶です。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							発信者情報開示がプロバイダによって処理されると、「発信者情報開示のお知らせ」という通知が郵送されてきます。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							どこの誰に、氏名・住所・メールアドレスが渡ったのかがこの通知内に記載されており、やっぱメーカーはこういうIPアドレス調査とかやってるんだなと納得です。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							選択や相手の判断によっては、民事訴訟・確率は低くとも刑事告訴になった場合のその先の人生観を考えたら、さくっと示談金を支払った方がいいという判断です。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							もちろんAVだけではなく、マンガ・映画なども損害賠償請求の対象です。開示請求が届いたとしても、人によっては完全無視をする選択を取ることもあるでしょうが、自分にはできませんでしたね。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							その後について詳細に書けませんが、電話で相手の弁護士と話して、示談金の減額ができるのか、どういう条件で和解になるのかなどを確認してお互いの主張を確認して事なきを得る状態にしました。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							今でもフルで動画を閲覧できるサイトが存在していたり、ビットトレントでのダウンロードができる状態なのだと思いますが、いくらでも調査されてしまう可能性はあるため、悪いことをして後悔するくらいなら、最初からやらないというのがいいと思います。
+						</p>
+						<p className='text-base text-gray-700 dark:text-gray-300'>
+							そのため、このサイトでは違法性がない情報で構成して、健全なアダルトを楽しんでもらえるようにしたいと思って始めています。
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default SenderInformation
--- src/app/layoutcomponents/Footer.tsx
+++ src/app/layoutcomponents/Footer.tsx
@@ -1,22 +1,31 @@
+import Link from 'next/link'
 // Footer.tsx
 import React from 'react'
-import Link from 'next/link'
 
 const Footer = () => {
 	return (
-		<footer className="bg-[#141921] py-8 mt-4">
-			<div className="container mx-auto px-4">
-				<div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
+		<footer className='bg-[#141921] py-8 mt-4'>
+			<div className='container mx-auto px-4'>
+				<div className='grid grid-cols-1 md:grid-cols-3 gap-8 mb-8'>
 					<div>
-						<h3 className="text-cyan-400 text-xl font-semibold mb-4">エロコメスト</h3>
-						<p className="text-gray-400 mb-4">最新のエロ動画を紹介しています。</p>
+						<h3 className='text-cyan-400 text-xl font-semibold mb-4'>エロコメスト</h3>
+						<p className='text-gray-400 mb-4'>
+							エロコメストは、エロ動画の良かった抜き情報を共有し合うこと、そして統計データを使った客観的な数字を使うことで、新たなエロ動画体験を提供することを目指しています。
+						</p>
 					</div>
 					<div>
-						<h3 className="text-cyan-400 text-xl font-semibold mb-4">リンク</h3>
-						<ul className="text-gray-400 space-y-2">
+						<h3 className='text-cyan-400 text-xl font-semibold mb-4'>リンク</h3>
+						<ul className='text-gray-400 space-y-2'>
 							<li>
-								<Link href="/about" className="hover:text-cyan-400">
+								<Link href='/about' className='hover:text-cyan-400'>
 									このサイトについて・お問い合わせ
+								</Link>
+							</li>
+							<li>
+								<Link
+									href='/request_for_disclosure_of_sender_information'
+									className='hover:text-cyan-400'>
+									発信者情報開示請求を受けたときの話
 								</Link>
 							</li>
 
@@ -49,8 +58,8 @@ const Footer = () => {
 						</ul> */}
 					</div>
 				</div>
-				<hr className="border-gray-700" />
-				<p className="text-center text-gray-500 mt-8">
+				<hr className='border-gray-700' />
+				<p className='text-center text-gray-500 mt-8'>
 					© {new Date().getFullYear()} エロコメスト. All rights reserved.
 				</p>
 			</div>
